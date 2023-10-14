c5f6b834e95c61053c4965abe1eb8b9c395428b6
Ajay Dillon <ajay.dillon@csuglobal.edu>
2023-10-13 22:51:09 -0700

    Adding Project to GitHub repo

---
+++ Add - Subtract.py
@@ -0,0 +1,14 @@
+#Gather Inputs from User
+
+num1 = int(input('Input first number:'))
+num2 = int(input('Input second number:'))
+
+#Calculate Sum and Difference
+
+add = num1 + num2
+sub = num1 - num2
+
+#Display Result
+
+print('The sum of these two numbers is.......... ', add)
+print('The difference of these two numbers is... ', sub)
---
+++ Mult - Divide.py
@@ -0,0 +1,14 @@
+#Gather Inputs from User
+
+num1 = int(input('Input first number:'))
+num2 = int(input('Input second number:'))
+
+#Calculate Multiplication and Division
+
+mult = num1 * num2
+div = num1 / num2
+
+#Display Result
+
+print('The multiplication of these two numbers is... ', mult)
+print('The division of these two numbers is......... ', div)
